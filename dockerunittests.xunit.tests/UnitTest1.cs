692eaadc166303e9aecfa3ab44389dcb4d7cd6a5
Haripraghash Subramaniam <hsubramaniam@alixpartners.com>
2019-02-27 07:47:10 +0000

    initial commit

--- dockerunittests.xunit.tests/UnitTest1.cs
+++ dockerunittests.xunit.tests/UnitTest1.cs
@@ -4,21 +4,21 @@ using dockerunittests.dataaccess;
 using dockerunittests.domain;
 using dockerunittests.services;
 using Moq;
-using NUnit.Framework;
+using Xunit;
 
-namespace Tests
+namespace dockerunittests.xunit.tests
 {
-    public class Tests
+    public class UnitTest1
     {
         private Mock<IProductsRepository> mockProductRepository;
         private string shouldFailTest = Environment.GetEnvironmentVariable("FAIL_TEST");
-        [SetUp]
-        public void Setup()
+
+        public UnitTest1()
         {
             this.mockProductRepository = new Mock<IProductsRepository>();
         }
 
-        [Test]
+        [Fact]
         public void Given_ProductId_ProductServices_Returns_Product()
         {
             this.mockProductRepository.Setup(x => x.Products).Returns(new List<Product>()
@@ -27,10 +27,10 @@ namespace Tests
             var productService = new ProductsService(this.mockProductRepository.Object);
 
             var actualResult = productService.GetById(1).Result;
-            Assert.IsTrue(actualResult.Id == 1);
+            Assert.True(actualResult.Id == 1);
         }
 
-        [Test]
+        [Fact]
         public void Given_ExistingProducts_ProductServices_Returns_All_Products()
         {
             this.mockProductRepository.Setup(x => x.Products).Returns(new List<Product>()
@@ -39,19 +39,19 @@ namespace Tests
             var productService = new ProductsService(this.mockProductRepository.Object);
 
             var actualResult = productService.GetAll().Result;
-            Assert.IsTrue(actualResult.Count == 2);
+            Assert.True(actualResult.Count == 2);
         }
 
-        [Test]
+        [Fact]
         public void Externally_Controlled_Test()
         {
             if (!string.IsNullOrEmpty(shouldFailTest))
             {
-                Assert.Fail("Test failed because FAIL_TEST environment variable is set");
+                Assert.False(false, "Test failed because FAIL_TEST environment variable is set");
             }
             else
             {
-                Assert.Pass();
+                Assert.True(true);
             }
         }
     }
